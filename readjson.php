07c415e88c2699a120857f96d056a2f82498ec25
catscarlet <i@catscarlet.com>
2015-07-18 18:24:36 +0800

    commit2

--- readjson.php
+++ readjson.php
@@ -1,14 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Let's get value</title>
+</head>
+
 <?php
 
+//$url = "http://www.btctrade.com/coin/rmb/btc/trust.js";
 $url = "trust.json";
 $contents = file_get_contents($url);
 $salelist = json_decode($contents);
 
 foreach ($salelist as $salemethod => $value1) {
     echo $salemethod . "<br>";
+    $value_sum = 0 ;
     foreach ($value1 as $saleindex => $value2)
     {
-        //echo $saleindex . "<br>";
+        echo "【" . $saleindex . "】";
         foreach ($value2 as $saleinfo => $saleamount)
         {
             if ($saleinfo == 'p') {
@@ -18,16 +26,16 @@ foreach ($salelist as $salemethod => $value1) {
             }
             if ($saleinfo == 'n') {
                 $value_group = $saleamount;
-                echo "$value_group , value ". $value_group*$mount_group . "<br>";
+                echo "$value_group , value ". $value_group*$mount_group ." . ";
+                $value_sum = $value_sum+  $value_group*$mount_group;
+                echo "and all beyond this value is ". $value_sum ;
+                echo "<br>";
             }
 
         }
 
 }
 
-
 }
-
-
 
  ?>
