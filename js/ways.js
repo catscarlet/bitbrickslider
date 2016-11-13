168663f60dbfca98ee554cc004f799f860a96b67
catscarlet <i@catscarlet.com>
2016-11-13 20:09:06 +0800

    add a new way

--- js/ways.js
+++ js/ways.js
@@ -72,3 +72,41 @@ function way01(input00) {
     };
     return result;
 }
+
+function way02(input00) {
+    //way-00 rmb@btcc - ltc@btcc - ltc@btc-e - btc@btc-e - btc@btctrade
+    //console.log(input_ltc);
+
+    var fee_ltc_btcc_out_fixed = 0.001;
+    var fee_ltc_btc_e_out_percent = 0.005;
+    var result;
+
+    output00 = stepwiseBuy(input00, rmb2btc_btctrade, '');
+    //console.log(output00);
+
+    output01 = withdrawWithFixedFee(output00, 0.005, '');
+    //console.log(output00);
+
+    output02 = stepwiseBuy(output01, ltc2btc_btce, '');
+    //console.log(output00);
+
+    output03 = withdrawWithPercentFee(output02, fee_ltc_btc_e_out_percent, '');
+    //console.log(output00);
+
+    output04 = stepwiseSell(output03, rmb2ltc_btctrade, '');
+    //console.log(output00);
+
+    result = {
+        'input00': input00,
+        'output': output04,
+        'data': {
+            'input00': input00,
+            'output00': output00,
+            'output01': output01,
+            'output02': output02,
+            'output03': output03,
+            'output04': output04,
+        }
+    };
+    return result;
+}
